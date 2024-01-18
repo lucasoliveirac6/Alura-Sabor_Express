e1e6f25d8d18d9e705a75e213e4e6fa4618b64e1
Lucas Carvalho <69948821+lucasoliveirac6@users.noreply.github.com>
2024-01-18 14:31:37 -0300

    Segundo modulo
    Primeiras aulas do segundo modulo

---
+++ Atividades/atividade_1.py
@@ -0,0 +1,7 @@
+# Imprimir em cada linha as letras da palavra ALURA
+for i in "ALURA":
+    print(i)
+
+# Mostrar o número de pi arredondado
+pi = 3.14159
+print(f"O valor arredondado de pi é: {pi:.2f}")
--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-print("Sabor Express\n")
+import math
+
+print("Sabor Express")
 
 print("1. Cadastrar restaurante")
 print("2. Listar restaurante")
 print("3. Ativar restaurante")
 print("4. Sair\n")
 
-opcao_escolhida = input("Escolha uma opção: ")
+opcao_escolhida = int(input("Escolha uma opção: "))
 
 print(f"Você escolheu a opção {opcao_escolhida}")
+
+if opcao_escolhida == 1:
+    print(f"Cadastrar restaurante")
+elif opcao_escolhida == 2:
+    print(f"Listar restaurantes")
+elif opcao_escolhida == 3:
+    print(f"Ativar restaurantes")
+else:
+    print(f"Encerrando o programa.")
